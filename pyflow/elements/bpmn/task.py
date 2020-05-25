b55a3e0d6a1bd5577726ac90784223a92f599b93
Rui Calheno <a78085@alunos.uminho.pt>
2020-05-25 22:43:14 +0100

    Add getter/setter to Task

--- pyflow/elements/bpmn/task.py
+++ pyflow/elements/bpmn/task.py
@@ -10,5 +10,37 @@ class Task (Node):
         self._completionQuantity = 1 
         self._default = None 
 
+    @property
+    def isForCompensation(self):
+        return self._isForCompensation 
+
+    @isForCompensation.setter 
+    def isForCompensation (self, isForCompensation):
+        self._isForCompensation = isForCompensation
+
+    @property
+    def startQuantity(self):
+        return self._startQuantity 
+
+    @startQuantity.setter 
+    def startQuantity (self, startQuantity):
+        self._startQuantity = startQuantity
+
+    @property
+    def completionQuantity(self):
+        return self._completionQuantity 
+
+    @completionQuantity.setter 
+    def completionQuantity (self, completionQuantity):
+        self._completionQuantity = completionQuantity
+
+    @property
+    def default(self):
+        return self._default 
+
+    @default.setter 
+    def default (self, default):
+        self._default = default
+
     def __repr__(self):
         return self._id
